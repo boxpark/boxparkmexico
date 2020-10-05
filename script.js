29228dfdabeb2373887e2f4c7421fe0c51893bb9
BOXPARKMEXICO <boxparkmexico@MacBook-Pro-de-BOXPARKMEXICO.local>
2020-10-05 11:55:27 -0700

    Primer commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if  ( 'serviceWorker'  en el  navegador )  {
-  navegador . serviceWorker . registrar ( './sw.js' )
-    . luego ( reg  =>  console . log ( 'Registro de SW exitoso' ,  reg ) )
-    . catch ( err  =>  console . warn ( 'Error al tratar de registrar el sw' ,  err ) )
-}
+    navegador . serviceWorker . registrar ( './sw.js' )
+      . luego ( reg  =>  console . log ( 'Registro de SW exitoso' ,  reg ) )
+      . catch ( err  =>  console . warn ( 'Error al tratar de registrar el sw' ,  err ) )
+  }
